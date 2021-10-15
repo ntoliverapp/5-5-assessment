f85fa9ce22fcb61b6d3aeb7afbcde2d588db511f
ntoliverapp <n.toliverapplication@gmail.com>
2021-10-15 11:19:51 -0700

    server index

--- server/index.js
+++ server/index.js
@@ -8,18 +8,25 @@ app.use(cors());
 
 app.use(express.json()); // When we want to be able to accept JSON.
 
-app.get("/api/compliment", (req, res) => {
-  const compliments = ["Gee, you're a smart cookie!",
-					 "Cool shirt!",
-					 "Your Javascript skills are stellar.",
-  ];
+const ctrl = require('./controller')
+app.get("/api/compliment", ctrl.getAllCompliments)
+
+
+// (req, res) => {
+//   const compliments = ["Gee, you're a smart cookie!",
+// 					 "Cool shirt!",
+// 					 "Your Javascript skills are stellar.",
+//   ];
 
   // choose random compliment
-  let randomIndex = Math.floor(Math.random() * compliments.length);
-  let randomCompliment = compliments[randomIndex];
+  // let randomIndex = Math.floor(Math.random() * compliments.length);
+  // let randomCompliment = compliments[randomIndex];
 
-  res.status(200).send(randomCompliment);
+  // res.status(200).send(randomCompliment);
   
-});
+// });
+
+
+
 
 app.listen(4000, () => console.log("Server running on 4000"));
